14337808d2935c3f073ee402f31704f1294f91cd
LukaL <Lukalursmanashvili398@gmail.com>
2023-10-28 17:03:52 +0400

    Did it

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Components/Header";
+import Meme from "./Components/Meme.jsx"
+function App(){
+  return(
+    <>
+    <Header />
+    <Meme />
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles.css'
+
+const Header = () => {
+  return (
+    <> 
+     <nav className='Header-background'>
+     <img src='/trollface.png'  className='img'/> 
+     <h2 className='Title' >Meme Generator</h2> 
+     <h4 className='SubTitle'> @Lukal    </h4> 
+     </nav>
+     </>
+
+  )
+}
+
+export default Header
